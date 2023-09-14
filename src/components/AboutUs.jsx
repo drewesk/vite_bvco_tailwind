56ee4ae0fb551585e9551a8d95cf083e51ac5a87
AE <drew@Andrews-MacBook-Pro.local>
2023-09-14 15:59:32 -0600

    edits

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,7 +5,7 @@ import Lottie from 'lottie-react'
 const AboutUs = () => {
   return (
       
-    <div id="one" className='max-w-[1400px] m-auto py-16 px-4 grid lg:grid-cols-2 gap-4 bg-black/20 '>
+    <div id="one" className='max-w-[1400px] m-auto py-16 px-4 grid lg:grid-cols-2 gap-4 bg-black/5 '>
 
       {/* Left Side */}
             <div className='grid grid-cols-2 grid-rows-1 h-[80vh] flex flex-col justify-center'>
@@ -16,16 +16,15 @@ const AboutUs = () => {
       <div className='flex flex-col h-full justify-center'>
         <h3 className='text-5xl md:text-6xl font-bold'>Our Story</h3>
         <p className='text-2xl py-6'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
-          nam?
+          What if you could have your cake and eat it too?
         </p>
         <p className='pb-6'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. In nostrum similique, asperiores cumque! Dignissimos earum, commodi harum quas id. Nobis magni ducimus officiis veniam atque, quas sed quaerat tempora minima facere, ratione recusandae dignissimos maxime accusantium vitae eius incidunt dolores?
+          We noticed a niche in the market, most vending companies are not taking advantage of premium markets local to the greater Denver-Boulder area. We offer both high quality options and natural ingredients for snack and drink as well as the "vending classics". Click below to learn more! 
         </p>
         <div>
           
           <a href='#contact'  className='font-bold text-3xl p-8'><button className='border-black mr-4 hover:shadow-xl'>
-            Send us a Message
+            Get started today!
           </button></a>
         </div>
       </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
       <div className='max-w-[1400px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8 px-4'>
 
         <div className='col-span-2 py-8 md:pt-2'>
-          <p className='font-bold uppercase'>Subscribe to our newsletters</p>
+          <p className='font-bold uppercase'>Subscribe to our newsletter</p>
           <p className='py-4'>
-            The Latest deals, articles and resources sent to your inbox weekly.
+            Deals, articles and resources sent to your inbox weekly.
           </p>
           <form className='flex flex-col sm:flex-row'>
             <input className='w-full p-2 mr-4 rounded-md mb-4' type='email' placeholder='Enter email..' />
@@ -19,21 +19,20 @@ const Footer = () => {
         </div>
                <br/>
                 <div>
-          <h6 className='font-bold uppercare pt-2'>Company</h6>
+          <h6 className='line-through font-bold uppercare pt-2'>Company</h6>
           <ul>
-            <li className='py-1'>Jobs</li>
-            <li className='py-1'>Press</li>
-            <li className='py-1'>Partners</li>
+            <li className='line-through py-1'>Jobs</li>
+            <li className='line-through py-1'>Press</li>
+            <li className='line-through py-1'>Partners</li>
           </ul>
         </div>
         <div>
-          <h6 className='font-bold uppercare pt-2'>Legal</h6>
+          <h6 className='font-bold uppercare pt-2 line-through'>Legal</h6>
           <ul>
-            <li className='py-1'>Claims</li>
-            <li className='py-1'>Privacy</li>
-            <li className='py-1'>Terms</li>
-            <li className='py-1'>Policies</li>
-            <li className='py-1'>Conditions</li>
+            <li className='line-through py-1'>Claims</li>
+            <li className='line-through py-1'>Privacy</li>
+            <li className='line-through py-1'>Terms</li>
+            <li className='line-through py-1'>Conditions</li>
           </ul>
         </div>
       </div>
